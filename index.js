ce6127ed5b9f129619e0719c5fa428feab83bd75
muhrifai7 <muhrifai554@gmail.com>
2019-08-30 14:31:44 +0700

    upPort

--- index.js
+++ index.js
@@ -22,5 +22,6 @@ app.group("/api/v1", (router) => {
     //another APIs goes here
 })
 
-
-app.listen(port, () => console.log(`Listening on port ${port}!`))
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Listening...`)
+})
